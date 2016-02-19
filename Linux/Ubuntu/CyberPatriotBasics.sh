8303d463b24fc221b1182568191aca8c253732e9
Bailey Kasin <KitsuneGamingYT@gmail.com>
2016-02-18 20:30:57 -0800

    Finalized the autoremove call.

--- Linux/Ubuntu/CyberPatriotBasics.sh
+++ Linux/Ubuntu/CyberPatriotBasics.sh
@@ -267,28 +267,35 @@ preserve_root_uid()
 
 remove_hacking_tools()
 {
-#http://tools.kali.org/tools-listing#webapplications
-    apt-get autoremove --purge airbase-ng acccheck ace-voip amap armitage asleap automater \
-	backdoor-factory bbqsql bed beef bing-ip2hosts binwalk bluelog bluemaho bluepot blueranger bluesnarfer bulk-extractor bully braa \
-	capstone casefile cdpsnarf chntpw cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch cisco-router-config cookie-cadger commix cowpatty \
-	crackle copy-router-config cuckoo \
-	dbpwaudit dc3dd ddrescue dff distorm3 dmitry dnmap dnsenum dnsmap dnsrecon dnstracer dnswalk doona dotdotpwn dumpzilla \
-	eapmd5pass enum4linux enumiax exploitdb extundelete \
-	fern-wifi-cracker fierce firewalk fragroute foremost \
-	galleta ghost-fisher giskismet go-lismero goofile gsad gsd gqrx guymager gr-scan \
-	hexorbase hping3 hydra \
-	inguma intrace iphone-backup-analyzer ismtp \
-	jboss-autopwn john jsql \
-	kalibrate-rtl killerbee kismet \
+    echo "$(tput setaf 2)Basically removing every program in Kali$(tput sgr0)"
+    apt-get autoremove --purge --force-yes -y airbase-ng acccheck ace-voip amap apache-users arachni android-sdk apktool arduino armitage asleap automater \
+	backdoor-factory bbqsql bed beef bing-ip2hosts binwalk blindelephant bluelog bluemaho bluepot blueranger bluesnarfer bulk-extractor \
+	bully burpsuite braa \
+	capstone casefile cdpsnarf cewl chntpw cisco-auditing-tool cisco-global-exploiter cisco-ocs cisco-torch cisco-router-config cmospwd \
+	cookie-cadger commix cowpatty crackle creddump crunch cryptcat cymothoa copy-router-config cuckoo cutycapt \
+	davtest dbd dbpwaudit dc3dd ddrescue deblaze dex2jar dff dhcpig dictstat dirb dirbuster distorm3 dmitry dnmap dns2tcp dnschef dnsenum \
+	dnsmap dnsrecon dnstracer dnswalk doona dos2unix dotdotpwn dradis dumpzilla \
+	eapmd5pass edb-debugger enum4linux enumiax exploitdb extundelete \
+	fern-wifi-cracker fierce fiked fimap findmyhash firewalk fragroute foremost funkload \
+	galleta ghost-fisher giskismet grabber go-lismero goofile gpp-decrypt gsad gsd gqrx guymager gr-scan \
+	hamster-sidejack hash-identifier hexinject hexorbase http-tunnel httptunnel hping3 hydra \
+	iaxflood inguma intrace inundator inviteflood ipv6-toolkit iphone-backup-analyzer intersect ismtp isr-evilgrade \
+	jad javasnoop jboss-autopwn jd-gui john johnny joomscan jsql \
+	kalibrate-rtl keepnote killerbee kismet keimpx \
 	linux-exploit-suggester ldb lynis \
-	maltego-teeth masscan mdk3 metagoofil metasploit mfcuk mfoc mfterm miranda multimon-ng \
-	netcat nmap ntop \
-	ohwurm openvas-administrator openvas-cli openvas-manager openvas-scanner oscanner \
-	p0f parsero pdf-parser pdfid pdgmail peepdf pixiewps powerfuzzer \
-	reaver recon-ng redfang regripper rtlsdr-scanner \
-	setoolkit sfuzz shellnoob sidguesser siparmyknife smtp-user-enum snmpcheck spooftootph sslcaudit sslsplit sslstrip sslyze sqlmap sqlninja sqlsus \
-	thc-ipv6 tnscmd10g theharverster tlssled twofi \
-	urlcrazy unix-privesc-check wifi-honey wifitap wifite wireshark volatility wol-e xplico yersinia
+	maltego-teeth magictree masscan maskgen maskprocessor mdk3 metagoofil metasploit mfcuk mfoc mfterm miranda mitmproxy multiforcer \
+	multimon-ng \
+	ncrack netcat nishang nipper-ng nmap ntop \
+	oclgausscrack ohwurm ollydpg openvas-administrator openvas-cli openvas-manager openvas-scanner oscanner \
+	p0f padbuster paros parsero patator pdf-parser pdfid pdgmail peepdf phrasendrescher pipal pixiewps plecost polenum policygen \
+	powerfuzzer powersploit protos-sip proxystrike pwnat \
+	rcrack rcrack-mt reaver rebind recon-ng redfang regripper responder ridenum rsmangler rtlsdr-scanner rtpbreak rtpflood rtpinsertsound \
+	rtpmixsound \
+	sakis3g sbd sctpscan setoolkit sfuzz shellnoob sidguesser siparmyknife sipp sipvicious skipfish slowhttptest smali smtp-user-enum \
+	sniffjoke snmpcheck spooftootph sslcaudit sslsplit sslstrip sslyze sqldict sqlmap sqlninja sqlsus statprocessor \
+	t50 termineter thc-hydra thc-ipv6 thc-pptp-bruter thc-ssl-dos tnscmd10g truecrack theharverster tlssled twofi \
+	u3-pwn uatester urlcrazy uniscan unix-privesc-check vega w3af webscarab webshag webshells webslayer websploit weevely wfuzz wifi-honey \
+	wifitap wifite wireshark winexe wpscan wordlists valgrind volatility voiphopper wol-e xspy xplico xsser yara yersinia zaproxy
     echo "$(tput setaf 2)Hacking tools should be removed now$(tput sgr0)"
 }
 
@@ -351,9 +358,9 @@ echo "$(tput setaf 2)Setting update settings$(tput sgr0)"
 set_update_settings
 
 echo "$(tput setaf 2)Updating$(tput sgr0)"
-apt-get update
+apt-get update --force-yes -y
 echo "$(tput setaf 2)Upgrading$(tput sgr0)"
-apt-get upgrade
+apt-get upgrade --force-yes -y
 
 echo "$(tput setaf 2)Firefox is sometimes a little bitch and won't update with everyone else, cause it wants to be a special little snowflake$(tput sgr0)"
 apt-get --purge --reinstall install firefox
