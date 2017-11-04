63caeb5ec8762e272cccb4f24abc2453cf40302f
Max Stanford-Taylor <moomanst@gmail.com>
2017-11-04 21:31:38 +0000

    Update CyberPatriotBasics.sh

--- Linux/Ubuntu/CyberPatriotBasics.sh
+++ Linux/Ubuntu/CyberPatriotBasics.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Shell script to do much of the basic CyberPatriot Ubuntu tasks.
 # Will run updates, create an HTML page with every user and folder and the amount of memory they each use and a list of default Ubuntu programs, force update Firefox and Libre Office, and set a password policy.
 # TODO: Add a list of default Ubuntu 14.04 processes.
 # Written for Ubuntu 14.04! No guarantee it will work in earlier or later versions!!
